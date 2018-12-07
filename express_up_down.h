7e014816a877c0da7738a5d7645eab158145db4d
Rahul Mahajan <36437143+mahajanrahul24@users.noreply.github.com>
2018-12-07 12:12:52 +0100

    update in prototype

--- express_up_down.h
+++ express_up_down.h
@@ -1,3 +1,3 @@
-#include"main.h"
-uint16_t Express_Signal(uint8_t IGNITION, uint8_t DOOR_STATUS, uint8_t ONE_TOUCH_EXPRESS_UP_DOWN);
+#include "main.h"
+uint8_t Express_Signal(uint8_t IGNITION, uint8_t DOOR_STATUS, uint8_t ONE_TOUCH_EXPRESS_UP_DOWN, uint8_t OBSTACLE_SENSOR);
 
