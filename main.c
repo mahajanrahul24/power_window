9801931e7478df735a01aebb8bc61f983b85b71f
Fahad Shakir <44699572+fahadsha@users.noreply.github.com>
2018-12-07 13:57:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "up.h"
+#include "down.h"
 #include "unity.h"
 
 
@@ -11,10 +12,20 @@ TEST_ASSERT_EQUAL_UINT8(1, Power_Window_UP_Mode(1, 0, 1));
 TEST_ASSERT_EQUAL_UINT8(0, Power_Window_UP_Mode(0, 0, 1));
 }
 
+void Test_DownWindow(void)
+{
+TEST_ASSERT_EQUAL_UINT8(2, windowDown(1, 1, 1));
+TEST_ASSERT_EQUAL_UINT8(2, windowDown(0, 1, 1));
+TEST_ASSERT_EQUAL_UINT8(2, windowDown(1, 0, 1));
+TEST_ASSERT_EQUAL_UINT8(0, windowDown(0, 0, 1));
+}
+
+
 int main(void)
 {
 UNITY_BEGIN();
 RUN_TEST(test_Window_UP);
+RUN_TEST(Test_DownWindow);  
 return UNITY_END();
 }
 
